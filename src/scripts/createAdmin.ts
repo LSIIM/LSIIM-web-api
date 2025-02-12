e213cea2848ea83f7b870a12f16da005a92b6814
dariel26 <asdfgh.daac@gmail.com>
2025-02-11 22:17:29 -0300

    Corrigindo comando yarn create-admin

--- src/scripts/createAdmin.ts
+++ src/scripts/createAdmin.ts
@@ -1,20 +1,19 @@
-// import { encryptPassword } from "../utils/encryption";
-// import prisma from "../prisma/prisma";
-
-// async function createDefaultAdmin() {
-//   try {
-//     await prisma.user.create({
-//       data: {
-//         name: "",
-//         email: "",
-//         password: await encryptPassword(""),
-//         role:"ADMIN"
-//       },
-//     });
-//     console.log("Usuário administrador criado!");
-//   } catch (err) {
-//     throw err;
-//   }
-// }
-
-// createDefaultAdmin();
+import { encryptPassword } from "../utils/encryption";
+import prisma from "../prisma/prisma";
+async function createDefaultAdmin() {
+    try {
+        await prisma.user.create({
+            data: {
+                name: "Super Usuário",
+                email: "super@usuario.com",
+                password: await encryptPassword("123"),
+                role: "ADMIN",
+                documento: "123",
+            },
+        });
+        console.log("Usuário administrador criado!");
+    } catch (err) {
+        throw err;
+    }
+}
+createDefaultAdmin();
